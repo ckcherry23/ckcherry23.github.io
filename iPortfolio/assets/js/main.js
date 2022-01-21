9df2396205a27b8f3c1b66189fb2d0c763e3ae08
Charisma Kausar <charisma.kausar@gmail.com>
2022-01-22 06:18:24 +0800

    Hide navbar in all displays

--- iPortfolio/assets/js/main.js
+++ iPortfolio/assets/js/main.js
@@ -97,7 +97,7 @@
   })
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
+   * Scroll with offset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
@@ -115,7 +115,7 @@
   }, true)
 
   /**
-   * Scroll with ofset on page load with hash links in the url
+   * Scroll with offset on page load with hash links in the url
    */
   window.addEventListener('load', () => {
     if (window.location.hash) {
