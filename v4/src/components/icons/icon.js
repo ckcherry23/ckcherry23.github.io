dff2b36b7845d04b97f38f31a978be3fcf37ef69
Charisma Kausar <charisma.kausar@gmail.com>
2022-06-28 00:23:35 +0800

    Add icons for languages

--- v4/src/components/icons/icon.js
+++ v4/src/components/icons/icon.js
@@ -4,16 +4,22 @@ import {
   IconAppStore,
   IconBookmark,
   IconCodepen,
+  IconCPlusPlus,
+  IconCSharp,
+  IconDart,
   IconExternal,
   IconFolder,
   IconFork,
   IconGitHub,
   IconInstagram,
+  IconJava,
+  IconJavaScript,
   IconLinkedin,
   IconLoader,
   IconLogo,
   IconMedium,
   IconPlayStore,
+  IconPython,
   IconStar,
   IconTwitter,
 } from '@components/icons';
@@ -26,6 +32,12 @@ const Icon = ({ name }) => {
       return <IconBookmark />;
     case 'Codepen':
       return <IconCodepen />;
+    case 'C++':
+      return <IconCPlusPlus />;
+    case 'C#':
+      return <IconCSharp />;
+    case 'Dart':
+      return <IconDart />;
     case 'External':
       return <IconExternal />;
     case 'Folder':
@@ -36,6 +48,10 @@ const Icon = ({ name }) => {
       return <IconGitHub />;
     case 'Instagram':
       return <IconInstagram />;
+    case 'Java':
+      return <IconJava />;
+    case 'JavaScript':
+      return <IconJavaScript />;
     case 'Linkedin':
       return <IconLinkedin />;
     case 'Loader':
@@ -46,6 +62,8 @@ const Icon = ({ name }) => {
       return <IconMedium />;
     case 'PlayStore':
       return <IconPlayStore />;
+    case 'Python':
+      return <IconPython />;
     case 'Star':
       return <IconStar />;
     case 'Twitter':
