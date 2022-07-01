88a6b03baec066d76e742131e866d9bed226f345
Charisma Kausar <charisma.kausar@gmail.com>
2022-07-02 00:02:34 +0800

    Add activities section template

--- v4/src/components/index.js
+++ v4/src/components/index.js
@@ -11,6 +11,7 @@ export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 export { default as Skills } from './sections/skills';
 export { default as Jobs } from './sections/jobs';
+export { default as Activities } from './sections/activities';
 export { default as Featured } from './sections/featured';
 export { default as Projects } from './sections/projects';
 export { default as Contact } from './sections/contact';
---
+++ v4/src/components/sections/activities.js
@@ -0,0 +1,363 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import { KEY_CODES } from '@utils';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+import { Icon } from '@components/icons';
+
+const StyledActivitiesSection = styled.section`
+  max-width: 700px;
+
+  .inner {
+    display: flex;
+
+    @media (max-width: 600px) {
+      display: block;
+    }
+
+    // Prevent container from jumping
+    @media (min-width: 700px) {
+      min-height: 340px;
+    }
+  }
+`;
+
+const StyledTabList = styled.div`
+  position: relative;
+  z-index: 3;
+  width: max-content;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+
+  @media (max-width: 600px) {
+    display: flex;
+    overflow-x: auto;
+    width: calc(100% + 100px);
+    padding-right: 50px;
+    margin-right: -50px;
+    margin-bottom: 30px;
+  }
+  @media (max-width: 480px) {
+    width: calc(100% + 50px);
+    padding-right: 25px;
+    margin-right: -25px;
+  }
+
+  li {
+    &:first-of-type {
+      @media (max-width: 600px) {
+        margin-right: 50px;
+      }
+      @media (max-width: 480px) {
+        margin-right: 25px;
+      }
+    }
+    &:last-of-type {
+      @media (max-width: 600px) {
+        padding-right: 50px;
+      }
+      @media (max-width: 480px) {
+        padding-right: 25px;
+      }
+    }
+  }
+`;
+
+const StyledTabButton = styled.button`
+  ${({ theme }) => theme.mixins.link};
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: var(--tab-height);
+  padding: 0 20px 2px;
+  border-right: 2px solid var(--lightest-navy);
+  background-color: transparent;
+  color: ${({ isActive }) => (isActive ? 'var(--rose)' : 'var(--slate)')};
+  font-family: var(--font-mono);
+  font-size: var(--fz-xs);
+  text-align: right;
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    padding: 0 15px 2px;
+  }
+  @media (max-width: 600px) {
+    ${({ theme }) => theme.mixins.flexCenter};
+    min-width: 120px;
+    padding: 0 15px;
+    border-right: 0;
+    border-bottom: 2px solid var(--lightest-navy);
+    text-align: center;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: var(--light-navy);
+  }
+`;
+
+const StyledHighlight = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 10;
+  width: 2px;
+  height: var(--tab-height);
+  border-radius: var(--border-radius);
+  background: var(--rose);
+  transform: translateY(calc(${({ activeTabId }) => activeTabId} * var(--tab-height)));
+  transition: transform 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
+  transition-delay: 0.1s;
+
+  @media (max-width: 600px) {
+    top: auto;
+    bottom: 0;
+    width: 100%;
+    max-width: var(--tab-width);
+    height: 2px;
+    margin-right: 50px;
+    transform: translateX(calc(${({ activeTabId }) => activeTabId} * var(--tab-width)));
+  }
+  @media (max-width: 480px) {
+    margin-right: 25px;
+  }
+`;
+
+const StyledTabPanels = styled.div`
+  position: relative;
+  width: 100%;
+  margin-right: 20px;
+
+  @media (max-width: 600px) {
+    margin-right: 0;
+  }
+`;
+
+const StyledTabPanel = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 10px 5px;
+
+  ul {
+    ${({ theme }) => theme.mixins.fancyList};
+  }
+
+  h3 {
+    margin-bottom: 2px;
+    font-size: var(--fz-xxl);
+    font-weight: 500;
+    line-height: 1.3;
+
+    .company {
+      color: var(--rose);
+    }
+  }
+
+  .range {
+    margin-bottom: 25px;
+    color: var(--light-slate);
+    font-family: var(--font-mono);
+    font-size: var(--fz-xs);
+  }
+
+  .stack-list {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    justify-content: right;
+  }
+
+  .stack-icon-name {
+    bottom: -1.3rem;
+    font-size: 10px;
+    font-family: var(--font-mono);
+    opacity: 0;
+    position: absolute;
+    transition: opacity .5s,color .5s;
+    white-space: nowrap;
+  }
+
+  .stack-icon:hover {
+    .stack-icon-name {
+      opacity: 1;
+    }
+
+    a:hover {
+        color: var(--rose);
+    }
+  }
+
+  .stack-icon {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 40px;
+    justify-content: center;
+    margin-bottom: 1.5rem;
+    margin-right: 1rem;
+    position: relative;
+    width: 40px;
+    z-index: 0;
+    max-height: 40px;
+    max-width: 40px;
+  }
+`;
+
+const Activities = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      activities: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/activities/" } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              company
+              location
+              range
+              url
+              techstack
+            }
+            html
+          }
+        }
+      }
+    }
+  `);
+
+  const activitiesData = data.activities.edges;
+
+  const [activeTabId, setActiveTabId] = useState(0);
+  const [tabFocus, setTabFocus] = useState(null);
+  const tabs = useRef([]);
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const focusTab = () => {
+    if (tabs.current[tabFocus]) {
+      tabs.current[tabFocus].focus();
+      return;
+    }
+    // If we're at the end, go to the start
+    if (tabFocus >= tabs.current.length) {
+      setTabFocus(0);
+    }
+    // If we're at the start, move to the end
+    if (tabFocus < 0) {
+      setTabFocus(tabs.current.length - 1);
+    }
+  };
+
+  // Only re-run the effect if tabFocus changes
+  useEffect(() => focusTab(), [tabFocus]);
+
+  // Focus on tabs when using up & down arrow keys
+  const onKeyDown = e => {
+    switch (e.key) {
+      case KEY_CODES.ARROW_UP: {
+        e.preventDefault();
+        setTabFocus(tabFocus - 1);
+        break;
+      }
+
+      case KEY_CODES.ARROW_DOWN: {
+        e.preventDefault();
+        setTabFocus(tabFocus + 1);
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+  };
+
+  return (
+    <StyledActivitiesSection id="activities" ref={revealContainer}>
+      <h2 className="numbered-heading">Where I’ve Contributed</h2>
+
+      <div className="inner">
+        <StyledTabPanels>
+          {activitiesData &&
+            activitiesData.map(({ node }, i) => {
+              const { frontmatter, html } = node;
+              const { title, url, company, range, techstack } = frontmatter;
+
+              return (
+                <CSSTransition key={i} in={activeTabId === i} timeout={250} classNames="fade">
+                  <StyledTabPanel
+                    id={`panel-${i}`}
+                    role="tabpanel"
+                    tabIndex={activeTabId === i ? '0' : '-1'}
+                    aria-labelledby={`tab-${i}`}
+                    aria-hidden={activeTabId !== i}
+                    hidden={activeTabId !== i}>
+                    <h3>
+                      <span>{title}</span>
+                      <span className="company">
+                        &nbsp;@&nbsp;
+                        <a href={url} className="inline-link">
+                          {company}
+                        </a>
+                      </span>
+                    </h3>
+
+                    <p className="range">{range}</p>
+
+                    <div dangerouslySetInnerHTML={{ __html: html }} />
+                    
+                    <div className="stack-list">
+                      {techstack && techstack.map((tech, i) =>
+                        <a className="stack-icon" key={tech}>
+                          <Icon name={tech} />
+                          <div className="stack-icon-name">{tech}</div>
+                        </a>
+                      )}
+                    </div>
+                  </StyledTabPanel>
+                </CSSTransition>
+              );
+            })}
+        </StyledTabPanels>
+
+        <StyledTabList role="tablist" aria-label="Job tabs" onKeyDown={e => onKeyDown(e)}>
+          {activitiesData &&
+            activitiesData.map(({ node }, i) => {
+              const { company } = node.frontmatter;
+              return (
+                <StyledTabButton
+                  key={i}
+                  isActive={activeTabId === i}
+                  onClick={() => setActiveTabId(i)}
+                  ref={el => (tabs.current[i] = el)}
+                  id={`tab-${i}`}
+                  role="tab"
+                  tabIndex={activeTabId === i ? '0' : '-1'}
+                  aria-selected={activeTabId === i ? true : false}
+                  aria-controls={`panel-${i}`}>
+                  <span>{company}</span>
+                </StyledTabButton>
+              );
+            })}
+          <StyledHighlight activeTabId={activeTabId} />
+        </StyledTabList>
+      </div>
+    </StyledActivitiesSection>
+  );
+};
+
+export default Activities;
