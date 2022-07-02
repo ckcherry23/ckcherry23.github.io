960639acca8b92f53d849985b6673719bc491312
Charisma Kausar <charisma.kausar@gmail.com>
2022-07-02 16:03:43 +0800

    Update footer source code link

--- v4/src/components/footer.js
+++ v4/src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/ckcherry23/ckcherry23.github.io')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <a href="https://github.com/ckcherry23/ckcherry23.github.io">
+          <div>Source Code</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
