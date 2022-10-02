acece35787760515fe5ee20b33a1429fb0f1245d
Charisma Kausar <charisma.kausar@gmail.com>
2022-10-03 03:48:46 +0800

    Save color ideas

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -248,7 +248,7 @@ const StyledProject = styled.li`
     @media (max-width: 768px) {
       grid-column: 1 / -1;
       height: 100%;
-      opacity: 0.25;
+      opacity: 0.15;
     }
 
     a {
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,9 +1,9 @@
 import { css } from 'styled-components';
-
+//new navy: #161616
 const variables = css`
   :root {
     --dark-navy: #020c1b;
-    --navy: #0a192f;
+    --navy: #0a192f; 
     --light-navy: #112240;
     --lightest-navy: #233554;
     --navy-shadow: rgba(2, 12, 27, 0.7);
